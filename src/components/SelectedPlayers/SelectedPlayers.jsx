85691d7261422ae600b6b9df888d4e9e6b1222b0
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-03-03 00:42:59 +0600

    added empty player conditional rendering

--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -2,36 +2,46 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 import Available from "../AvailablePlayers/Available";
 
-const SelectedPlayers = ({ selectedPlayer,handleRemoveCart,setToggle}) => 
-    {
-
-        
+const SelectedPlayers = ({ selectedPlayer, handleRemoveCart, setToggle }) => {
   return (
     <div className="max-w-6xl mx-auto my-10 md:px-0 px-5">
-      {selectedPlayer.map((selected) => (
-        <div>
-          <div className="card bg-base-300 text-gray-700 p-5 my-5">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center gap-3">
-                <img
-                  className="w-20 object-cover bg-gray-300 p-2 rounded-2xl"
-                  src={selected.player_image}
-                  alt=""
-                />
-                <div>
-                  <h1 className="font-semibold text-xl">{selected.player_Name}</h1>
-                  <p>{selected.boweling_style}</p>
+      {selectedPlayer.length === 0 ? (
+        <p className="text-center text-gray-500 mt-5">Select your player</p>
+      ) : (
+        selectedPlayer.map((selected) => (
+          <div key={selected.id}>
+            <div className="card bg-base-300 text-gray-700 p-5 my-5">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-3">
+                  <img
+                    className="w-20 object-cover bg-gray-300 p-2 rounded-2xl"
+                    src={selected.player_image}
+                    alt={selected.player_Name}
+                  />
+                  <div>
+                    <h1 className="font-semibold text-xl">
+                      {selected.player_Name}
+                    </h1>
+                    <p>{selected.boweling_style}</p>
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <button onClick={()=>handleRemoveCart(selected.id)}  className="bg-red-300 p-2 rounded-full hover:bg-red-500"><MdDelete  size={30}/></button>
+                <div>
+                  <button
+                    onClick={() => handleRemoveCart(selected.id)}
+                    className="bg-red-300 p-2 rounded-full hover:bg-red-500"
+                  >
+                    <MdDelete size={30} />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-      <button onClick={()=>setToggle(true)} className="btn btn-success mt-5">Add More</button>
+        ))
+      )}
+      <button onClick={() => setToggle(true)} className="btn btn-success mt-5">
+        Add More
+      </button>
     </div>
   );
 };
